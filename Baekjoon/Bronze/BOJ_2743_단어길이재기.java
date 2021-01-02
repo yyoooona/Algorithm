3b0503a5afc34b31f0a7d9e3a4f34085824308e6
yyoooona <yyoooona@gmail.com>
2021-01-02 18:19:12 +0900

    210102 BOJ 2743 단어길이재기

---
+++ Baekjoon/Bronze/BOJ_2743_단어길이재기.java
@@ -0,0 +1,12 @@
+package baekjoon.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2743_단어길이재기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().length());
+	}
+}
